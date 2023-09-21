4596be91b2459cd481be9a7d6d35fb4bc984b0ef
valike23 <ludike23@gmail.com>
2023-09-21 01:59:27 +0100

    Update the interval for updating naira rates from 3 minutes to 10 minutes.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { updateCoin, updateNairaRates } from "./services/updateCoins";
 
 
- setInterval(updateNairaRates,1000 * 60  * 3);
+ setInterval(updateNairaRates,1000 * 60  * 10);
  setInterval(updateCoin,1000 * 60 * 10);
