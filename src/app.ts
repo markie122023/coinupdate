438c2d6a28bd92cc5ca8e4fca1268b5fc9c86935
valike23 <ludike23@gmail.com>
2023-10-15 18:26:31 +0100

    Added Fastify web framework and created a simple route.

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,20 @@ import { updateCoin, updateNairaRates } from "./services/updateCoins";
 
  setInterval(updateNairaRates,1000 * 60  * 10);
  setInterval(updateCoin,1000 * 60 * 10);
+ const fastify = require('fastify')();
+
+// Define a route
+fastify.get('/', (request, reply) => {
+  reply.send({ message: 'Hello, Fastify!' });
+});
+
+// Start the server
+fastify.listen(3000, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server is now listening on ${address}`);
+});
+
+ 
