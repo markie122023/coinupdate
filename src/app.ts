c065f5b65dda27610129f33c3fb0d86e96b00767
valike23 <ludike23@gmail.com>
2023-10-15 18:35:08 +0100

    Changed port binding from 3000 to 0 and IP binding from 0.0.0.0 to 127.0.0.1.

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,13 @@ fastify.get('/', (request, reply) => {
 });
 
 // Start the server
-fastify.listen(3000, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server is now listening on ${address}`);
-});
+// Set the port to 0, which tells the operating system to choose an available port.
+fastify.listen(0, '127.0.0.1', (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server is now listening on port ${address.port}`);
+  });
 
  
